dcabdeaf032de2f741a3111d2f6bf414da212928
Kyle Reynolds <kyle.reynolds@bridgerphotonics.com>
2024-02-01 12:03:23 -0700

    running out of limit lol

--- build-gdal_sim.sh
+++ build-gdal_sim.sh
@@ -26,7 +26,7 @@ cd build_$OS
 
 # Run cmake with the required options for simulator
 cmake -DCMAKE_TOOLCHAIN_FILE=$CMTOOLCHAIN \
-      -DPLATFORM=SIMULATORARM64 \
+      -DPLATFORM=$OS \
       -DENABLE_BITCODE=OFF \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DBUILD_SHARED_LIBS=OFF \
@@ -37,15 +37,8 @@ cmake -DCMAKE_TOOLCHAIN_FILE=$CMTOOLCHAIN \
       -DSQLITE3_LIBRARY=$PREFIX/lib/libsqlite3.a \
       -DIconv_INCLUDE_DIR=$SDKPATH/usr \
       -DIconv_LIBRARY=$SDKPATH/usr/lib/libiconv.tbd \
-      -DOPENSSL_ROOT_DIR=${OPENSSL_PREFIX} \
-      -DOPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR} \
-      -DOPENSSL_CRYPTO_LIBRARY=${OPENSSL_LIB_DIR}/libcrypto.a \
-      -DOPENSSL_SSL_LIBRARY=${OPENSSL_LIB_DIR}/libssl.a \
-      -DZSTD_INCLUDE_DIR=${ZSTD_PREFIX}/include \
-      -DZSTD_LIBRARY=${ZSTD_PREFIX}/lib/libzstd.a \
       -DCMAKE_BUILD_TYPE=Release \
       ..
-
 # Build and install
 cmake --build .
 cmake --build . --target install
